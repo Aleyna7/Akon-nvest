cba308581330411b461ea6f59ce166191b8345bf
Aleyna Öz <125676714+Aleyna7@users.noreply.github.com>
2023-11-19 18:37:30 +0300

    Concrete Kısmı oluşturuldu BussinessLayarda

---
+++ BussinessLayer/Concrete/CityManager.cs
@@ -0,0 +1,52 @@
+﻿using BussinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class CityManager : GenericService<City>
+    {
+
+        ICityRepository _cityRepository;
+
+        public CityManager(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository;
+        }
+        public void Add(City p)
+        {
+            _cityRepository.Add(p);
+        }
+
+        public void Delete(City p)
+        {
+          _cityRepository.Delete(p);
+        }
+
+        public City GetById(int id)
+        {
+           return _cityRepository.GetById(id);
+        }
+
+        public List<City> List()
+        {
+           return _cityRepository.List();
+        }
+
+        public List<City> List(Expression<Func<City, bool>> filter)
+        {
+            return _cityRepository.List(filter);
+        }
+
+        public void Update(City p)
+        {
+            _cityRepository.Update(p);
+        }
+    }
+}
