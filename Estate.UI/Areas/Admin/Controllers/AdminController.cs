418ce4f2fcd9be8f6f12fc5cbae59280db786180
Aleyna Öz <125676714+Aleyna7@users.noreply.github.com>
2023-11-26 21:49:09 +0300

    .

--- Estate.UI/Areas/Admin/Controllers/AdminController.cs
+++ Estate.UI/Areas/Admin/Controllers/AdminController.cs
@@ -3,8 +3,6 @@ using Estate.UI.Areas.Admin.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.DataProtection;
 
 namespace Estate.UI.Areas.Admin.Controllers
 {
@@ -12,25 +10,19 @@ namespace Estate.UI.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
-        
-        private readonly UserManager <UserAdmin> _userManager;
-        private readonly  SignInManager <UserAdmin> _signInManager;
-        // Şifreleme için DataProtectionProvider ekleme
-        private readonly IDataProtectionProvider _dataProtectionProvider;
 
-        public AdminController(UserManager<UserAdmin> userManager, SignInManager<UserAdmin> signInManager, IDataProtectionProvider dataProtectionProvider)
+        private UserManager<UserAdmin> _userManager;
+        private SignInManager<UserAdmin> _signInManager;
+
+        public AdminController(UserManager<UserAdmin> userManager, SignInManager<UserAdmin> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
-            _dataProtectionProvider = dataProtectionProvider;
         }
 
         public IActionResult Index()
         {
-            // Tüm kullanıcıları al
-            var allUsers = _userManager.Users.ToList();
-
-            return View(allUsers);
+            return View();
         }
         [AllowAnonymous]
         public IActionResult Login()
@@ -47,23 +39,17 @@ namespace Estate.UI.Areas.Admin.Controllers
             {
                 return View(model);
             }
-
             var user = await _userManager.FindByNameAsync(model.Username);
             if (user == null)
             {
-                ModelState.AddModelError("", "Kullanıcı adı veya şifre hatalı.");
+                ModelState.AddModelError("", "Bilgiler Uyuşmuyor");
             }
-
             var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
 
             if (result.Succeeded)
             {
-                // Kullanıcı bilgilerini şifreleyerek session'a ekleme
-                var protector = _dataProtectionProvider.CreateProtector("Estate.UI.AdminController");
-                var encryptedId = protector.Protect(user.Id);
-                HttpContext.Session.SetString("Id", encryptedId);
-                HttpContext.Session.SetString("FullName", user.FullName);
-
+                HttpContext.Session.SetString("Id",user.Id);
+                HttpContext.Session.SetString("FullName",user.FullName);
                 return RedirectToAction("Index");
             }
 
